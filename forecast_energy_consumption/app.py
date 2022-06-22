9648721dcc98d364d1689f76f86dfa3d09cb2777
LDEMANGE <demange.louis@hotmail.fr>
2022-06-22 14:52:12 +0200

    ok

--- forecast_energy_consumption/app.py
+++ forecast_energy_consumption/app.py
@@ -8,6 +8,8 @@ from PIL import Image
 from datetime import timedelta
 from forecast_energy_consumption.dataprep import X_y_train_test
 from forecast_energy_consumption.predict import predict_output
+import pandas as pd
+import numpy as np
 
 #TODO
 #remonter input date 1 year, graph, 
@@ -46,10 +48,12 @@ from forecast_energy_consumption.predict import predict_output
 url = 'http://127.0.0.1:5000/predict' #'https://taxifare.lewagon.ai/predict' (exercice 1, jeudi)
 
 
-inpdate1 = st.date_input(label= "Starting date :", value= datetime.date(2013, 1, 1), min_value=datetime.date(2013, 1, 1), max_value=datetime.date(2022, 4, 30))
+inpdate1 = st.date_input(label= "Starting date :", value= datetime.date(2015, 1, 1), min_value=datetime.date(2013, 1, 1), max_value=datetime.date(2022, 4, 30))
 
 date1 = inpdate1#value()
 date2 = date1 + timedelta(days = 14) 
+
+st.write(type(str(date1)))
 
 #date2 = st.date_input(label= "Ending date :", value= datetime.date(2022, 4, 30), min_value=datetime.date(2013, 1, 1), max_value=datetime.date(2022, 4, 30))
 
@@ -58,6 +62,18 @@ st.write('The energy consumption forecast from',date1,'to',date2,'is :')
 #if date2 > datetime.date(2022, 4, 30):
 #    st.error('The ending date should not be higher than 2022/04/30')
 
-X_train,y_train,X_test,y_test = X_y_train_test(str(date1), str(date2))
+#X_train,y_train,X_test,y_test = X_y_train_test(str(date1), str(date2))
+
+
+X_train,y_train,X_test,y_test = X_y_train_test(str(date1), 14)
+
+st.write(X_train)
 
 y_pred = predict_output(X_test,y_test, metric = True)
+
+st.write(type(y_pred))
+
+st.write(y_pred)
+
+df = pd.DataFrame(y_pred,columns=['y_pred'])
+st.line_chart(df)
