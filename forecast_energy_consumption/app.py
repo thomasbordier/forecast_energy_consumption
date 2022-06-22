6274e6981299d86b49f67a0cdfcf724db1d4669e
LDEMANGE <demange.louis@hotmail.fr>
2022-06-22 19:32:47 +0200

    for merge

--- forecast_energy_consumption/app.py
+++ forecast_energy_consumption/app.py
@@ -1,3 +1,4 @@
+from jupyter_server import DEFAULT_STATIC_FILES_PATH
 from pandas import timedelta_range
 import streamlit as st
 import datetime
@@ -13,6 +14,9 @@ import numpy as np
 from forecast_energy_consumption.consumption_history import consumption_history
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.express as px
+from forecast_energy_consumption.knn_production import knn_production
+from forecast_energy_consumption.main import main
 
 #TODO
 #remonter input date 1 year, graph, 
@@ -62,25 +66,48 @@ st.write('The energy consumption forecast from',date1,'to',date2,'is :')
 #date_test = pd.DataFrame(['2015-01-01','2015-01-05']).set_index(0).asfreq('D')
 date_test = pd.DataFrame([date1,date2]).set_index(0).asfreq('D') 
     
-X_train,y_train,X_test,y_test,df_train = X_y_train_test(str(date1), 14)
+#X_train,y_train,X_test,y_test,df_train = X_y_train_test(str(date1), 14)
 
-y_pred, mape_pred = predict_output(X_test,y_test, metric = True)
+df_train, X_test, y_test, predictions, mape = main('xgb',date1, 14)
+
+#y_pred, mape_pred = predict_output(X_test,y_test, metric = True)
 
 date_train, prod_history = consumption_history(str(date1))
 
-fig1 = plt.figure(figsize=(10, 4))
-plt.plot(pd.to_datetime(date_train['Date']),date_train['Consommation (MW)'], label="y_true")
-plt.title("Energy consumption 1 year before",fontsize=14, fontweight='bold')
-plt.legend()
-st.pyplot(fig1)
 
-################ GR
-fig2 = plt.figure(figsize=(10, 4))
-plt.plot(date_test.index,y_pred, label="y_pred")
-plt.title("Energy consumption forecast for the 14 next days",fontsize=14, fontweight='bold')
-plt.legend()
-st.pyplot(fig2)
+############### One year before
+#fig = plt.figure(figsize=(10, 4))
+#plt.plot(pd.to_datetime(date_train['Date']),date_train['Consommation (MW)'], label="y_true")
+#plt.title("Energy consumption 1 year before",fontsize=14, fontweight='bold')
+#plt.legend()
+#st.pyplot(fig1)
+
+#df = px.data.gapminder().query("country=='Canada'")
+fig1 = plt.figure(figsize=(10, 4))
+fig1 = px.line(date_train, x="Date", y="Consommation (MW)", title='Energy consumption 1 year before')
+st.plotly_chart(fig1)
+#fig1.show()
+
 ################
 
-st.write('erreur moyenne:',round(mape_pred,2),'%')
+RPA = prod_history
 
+fig = px.pie(values = np.array(prod_history.values).tolist()[0],names = prod_history.columns)
+#fig.show()
+st.plotly_chart(fig)
+
+
+
+################ GR
+
+fig2 = plt.figure(figsize=(10, 4))
+fig2 = px.line(x=date_test.index, y=predictions, title='Energy consumption forecast for the 14 next days')
+st.plotly_chart(fig2)
+
+################
+
+st.write('erreur moyenne:',round(mape,4),'%')
+
+#knn_production(df_train, X_test, y_pred, Date_debut_test)
+date_list, thermique_list, eolien_list, solaire_list, hydraulique_list, bioenergies_list, ech_physiques_list = knn_production(df_train, X_test, y_pred, date1)
+#fig = px.line(df_energy_weather[['Consommation (MW)','Ech. physiques (MW)']]['2018':'2020'].resample('M').mean(),title='Evolution des échanges physiques par rapport à nos besoins en énergie entre 2018 et 2020')
