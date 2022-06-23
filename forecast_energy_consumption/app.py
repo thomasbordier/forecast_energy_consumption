9818549e1d3d83034206ff422d4351b11ee82cea
LDEMANGE <demange.louis@hotmail.fr>
2022-06-23 12:07:53 +0200

    save for merge

--- forecast_energy_consumption/app.py
+++ forecast_energy_consumption/app.py
@@ -19,12 +19,6 @@ from forecast_energy_consumption.knn_production import knn_production
 from forecast_energy_consumption.main import main
 import plotly.graph_objects as go
 
-#TODO
-#remonter input date 1 year, graph, 
-# camebert production
-# prediction 14 jours
-# production thomas
-
 '''
 # Energy consumption forecast 
 '''
@@ -60,20 +54,23 @@ date1 = st.date_input(label= "Starting date :", value= datetime.date(2020, 1, 1)
 
 date2 = date1 + timedelta(days = 13) 
 
-#date2 = st.date_input(label= "Ending date :", value= datetime.date(2022, 4, 30), min_value=datetime.date(2013, 1, 1), max_value=datetime.date(2022, 4, 30))
-
-st.write('The energy consumption forecast from',date1,'to',date2,'is :')
-
-#date_test = pd.DataFrame(['2015-01-01','2015-01-05']).set_index(0).asfreq('D')
 date_test = pd.DataFrame([date1,date2]).set_index(0).asfreq('D') 
 
 df_train, X_test, y_test, predictions, mape = main('xgb',str(date1), 14)
 
 date_train, prod_history = consumption_history(str(date1))
 
-st.markdown("<h1 style='text-align: center; color: black;'>Energy consumption forecast 1 year before</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: black;'>Energy consumption forecast 1 year before</h2>", unsafe_allow_html=True)
 fig1 = plt.figure(figsize=(10, 4))
-fig1 = px.line(date_train, x="Date", y="Consommation (MW)")#, title='Energy consumption 1 year before')
+fig1 = px.line(date_train, x="Date", y="Consommation (MW)", title='Energy consumption 1 year in PACA before')#,title_x = 5)#,title_size = 10)
+
+import textwrap
+split_text = textwrap.wrap('This is a very long title and it would be great to have it on three lines', 
+                            width=30)
+
+fig1.update_layout(title_text=f"Consommation d'énergie \n entre le {date1} et le {date2}", title_x=0.5,title_y=0.85, font=dict(family="Courier New, monospace",size=18,color="RebeccaPurple"))#,fontsize = 10)
+#fig1.update_layout(title_text=split_text, title_x=0.5,title_y=0.85, font=dict(family="Courier New, monospace",size=18,color="RebeccaPurple"))#,fontsize = 10)
+
 st.plotly_chart(fig1)
 
 RPA = prod_history
@@ -81,6 +78,8 @@ RPA = prod_history
 fig2 = px.pie(values = np.array(prod_history.values).tolist()[0],names = prod_history.columns)
 
 st.plotly_chart(fig2)
+
+st.write('The energy consumption forecast from',date1,'to',date2,'is :')
 
 fig3 = plt.figure(figsize=(10, 4))
 fig3 = px.line(x=date_test.index, y=predictions)#, title='Energy consumption forecast for the 14 next days')
